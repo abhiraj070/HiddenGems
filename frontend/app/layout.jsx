1c7f81eb1df4ac55d851a6e94101cc2927718ddd
Abhiraj Sharma <iamabhirajsharma@gmail.com>
2025-11-23 01:42:21 +0530

    imp fixes

--- frontend/app/layout.jsx
+++ frontend/app/layout.jsx
@@ -1,8 +1,4 @@
-import { Geist, Geist_Mono } from "next/font/google"
 import "./globals.css"
-
-const _geist = Geist({ subsets: ["latin"] })
-const _geistMono = Geist_Mono({ subsets: ["latin"] })
 
 export const metadata = {
   title: "HiddenGems - Discover Hidden Locations",
