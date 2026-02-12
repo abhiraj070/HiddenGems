62154644c8eac7bc4bc20e9d670e0c47be5871e4
Abhiraj Sharma <iamabhirajsharma@gmail.com>
2026-02-12 19:33:11 +0530

    fix4

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,7 +5,7 @@ import cors from 'cors'
 const app=express()
 app.use(cors({
     credentials: true,
-    origin: "http://localhost:3000"
+    origin: "https://hidden-gems-859h.vercel.app"
 }))
 
 app.use(cookieParser())
